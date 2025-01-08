f246e3dc02913eae2653df957e50eeea815a9a13
kkufieta <katharina.kufieta@gmail.com>
2025-01-08 16:54:35 -0500

    Create lib folder & move code

--- python/patterns/reverse_linked_list.py
+++ python/patterns/reverse_linked_list.py
@@ -1,38 +1,3 @@
-class LinkedListNode:
-
-    def __init__(self, data, next=None):
-        self.data = data
-        self.next = next
-
-
-class LinkedList:
-
-    def __init__(self, list=None):
-        self.head = None
-
-        if list:
-            self.create_from_array(list)
-
-    def insert_node_before_head(self, node):
-        if self.head:
-            node.next = self.head
-
-        self.head = node
-
-    def create_from_array(self, arr):
-        for val in arr[::-1]:
-            self.insert_node_before_head(LinkedListNode(val))
-
-    def as_array(self):
-        arr = []
-        node = self.head
-        while node:
-            arr.append(node.data)
-            node = node.next
-
-        return arr
-
-
 def reverse_linked_list(linked_list):
     prev = None
     current = linked_list.head
--- python/patterns/test_reverse_linked_list.py
+++ python/patterns/test_reverse_linked_list.py
@@ -1,5 +1,6 @@
 import pytest
-import reverse_linked_list as ll
+import reverse_linked_list as rll
+from lib import linked_list as ll
 
 @pytest.mark.parametrize("nums_list", [
     [1, 2, 3, 4, 5],
@@ -12,30 +13,7 @@ import reverse_linked_list as ll
     [],
 ])
 
-class TestLinkedList:
-    def test_linked_list(self, nums_list):
-        linked_list = ll.LinkedList(nums_list)
-        assert linked_list.as_array() == nums_list
-
-    def test_reverse_linked_list(self, nums_list):
-        linked_list = ll.LinkedList(nums_list)
-        ll.reverse_linked_list(linked_list)
-        assert linked_list.as_array() == nums_list[::-1]
-
-
-
-def test_linked_list_node():
-    val_1 = 5
-    node_1 = ll.LinkedListNode(val_1)
-    assert node_1 != None
-    assert node_1.data == val_1
-    assert node_1.next == None
-
-    val_2 = -1
-    node_2 = ll.LinkedListNode(val_2, node_1)
-    assert node_2 != None
-    assert node_2.data == val_2
-    assert node_2.next == node_1
-
-
-
+def test_reverse_linked_list(nums_list):
+    linked_list = ll.LinkedList(nums_list)
+    rll.reverse_linked_list(linked_list)
+    assert linked_list.as_array() == nums_list[::-1]
