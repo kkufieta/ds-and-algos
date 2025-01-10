f56ca4ee847dc198cdee83ba5065d1c4f8ea5ba1
kkufieta <katharina.kufieta@gmail.com>
2025-01-10 15:07:40 -0500

    Add tree tests

--- python/patterns/lib/test_tree.py
+++ python/patterns/lib/test_tree.py
@@ -11,6 +11,13 @@ from lib import tree
     [3, 9, 20, 15, 7],
     [1, 2],
     [9, 7, None, None, 1, 8, 10, None, 12],
+    [1, 2, 3, None, None, 4, 5, 6],
+    [3, 9, 2, 0, None, None, 15, 7],
+    [3, None, 20, None, 7],
+    [5, 6, 7, None, None, 8, 9],
+    [5, None, 6, None, 7, None, 8, None, 9],
+    [4, 10, 20, None, None, 15, 7],
+    [1, 2, 3, 4, 5, None, None, 8, None, 9, None, 10, None, 11],
 ])
 
 def test_tree_list_funcs(bt_list):
