53db2abc7ae39c13f8e67388f41393c41e7cddfd
kkufieta <katharina.kufieta@gmail.com>
2025-01-11 20:36:27 -0500

    Add swap numbers without tmp

--- python/patterns/test_bitwise_fun.py
+++ python/patterns/test_bitwise_fun.py
@@ -1,4 +1,5 @@
 import pytest
+import random
 from bitwise_fun import *
 
 @pytest.mark.parametrize("str1, str2, index", [
@@ -21,3 +22,20 @@ from bitwise_fun import *
 
 def test_find_difference(str1, str2, index):
     assert find_difference(str1, str2) == index
+
+def test_swap_without_extra_space():
+    x, y = 0, 0
+    expect_y, expect_x = swap_without_extra_space(x, y)
+    assert expect_x == 0
+    assert expect_y == 0
+
+    x, y = 1, 0
+    expect_y, expect_x = swap_without_extra_space(x, y)
+    assert expect_x == 1
+    assert expect_y == 0
+
+    for i in range(100):
+        x, y = random.randint(0, 10000), random.randint(0, 10000)
+        expect_y, expect_x = swap_without_extra_space(x, y)
+        assert expect_x == x
+        assert expect_y == y
