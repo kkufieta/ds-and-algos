133e90c7d46e01b669bd07b09b78d6224aae985e
kkufieta <katharina.kufieta@gmail.com>
2025-02-09 19:23:21 -0500

    Add zigzag implementation

--- python/daily-coding-problems/strings/zigzaglines.py
+++ python/daily-coding-problems/strings/zigzaglines.py
@@ -1,2 +1,35 @@
-def zigzaglines(s):
-    return s
+def zigzaglines(s, k):
+    if s == "" or k == 0:
+        return ""
+    if len(s) == 1 or k == 1:
+        return s
+
+    zz = ""    
+    for i in range(min(len(s), k)):
+        num_spaces_1 = (k - 2 - i) * 2 + 1
+        num_spaces_2 = (i - 1) * 2 + 1
+        if num_spaces_1 < 0:
+            num_spaces_1 = num_spaces_2
+        if num_spaces_2 < 0:
+            num_spaces_2 = num_spaces_1
+
+
+        zz += " " * i
+        j = i
+        while j < len(s):
+            zz += s[j]
+            j += num_spaces_1 + 1
+            if j >= len(s):
+                break
+            zz += " " * num_spaces_1
+
+            zz += s[j]
+            j += num_spaces_2 + 1
+            if j >= len(s):
+                break
+            zz += " " * num_spaces_2
+        
+        zz += "\n"
+
+
+    return zz[:-1]
