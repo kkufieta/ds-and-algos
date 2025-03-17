55730ec8d53403361d3139133607e04fcac9c438
kkufieta <katharina.kufieta@gmail.com>
2025-03-17 09:45:27 -0400

    Optimize knapsack

--- python/patterns/knapsack.py
+++ python/patterns/knapsack.py
@@ -1,8 +1,8 @@
 def knapsack_0_1(capacity, weights, values):
     vals = [0] * (capacity + 1)
 
-    for w, v in zip(weights, values):
+    for w, v in zip(weights[:-1], values[:-1]):
         for c in range(capacity, w-1, -1):
             vals[c] = max(vals[c], v + vals[c-w])
 
-    return vals[-1]
+    return max(vals[-1], values[-1] + vals[-1 - weights[-1]])
