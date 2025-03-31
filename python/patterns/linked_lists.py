f2a5709535670a248ef5ceedc474e385566a6992
kkufieta <katharina.kufieta@gmail.com>
2025-03-31 15:38:12 -0400

    Add reverse_2nd_half

--- python/patterns/linked_lists.py
+++ python/patterns/linked_lists.py
@@ -15,4 +15,20 @@ def reverse_linked_list(linked_list):
 
 # Reverse the 2nd half of a singly linked list
 def reverse_2nd_half(linked_list):
-    return 
+    if not linked_list.head or not linked_list.head.next:
+        return
+
+    end_of_1st_half, p_slow, p_fast = None, linked_list.head, linked_list.head
+    while p_fast != None and p_fast.next != None:
+        end_of_1st_half = p_slow
+        p_slow = p_slow.next
+        p_fast = p_fast.next.next
+
+    previous, current = None, p_slow
+    while current:
+        next_ = current.next
+        current.next = previous
+        previous = current
+        current = next_
+
+    end_of_1st_half.next = previous
