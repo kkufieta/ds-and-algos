ecbce0fbfe502ce2d4a52becaee870b66681f1b2
kkufieta <katharina.kufieta@gmail.com>
2025-03-30 20:32:01 -0400

    Add is_majority

--- python/patterns/modified_binary_search.py
+++ python/patterns/modified_binary_search.py
@@ -14,3 +14,57 @@ def binary_search(nums, target):
             right = mid - 1
 
     return -1
+
+# Check if a number is the majority element in a sorted array.
+def is_majority(nums, target):
+    if len(nums) == 0:
+        return False
+
+    mid = lambda l, r: l + (r-l)//2
+
+    first_found, leftmost, rightmost = None, None, None
+    l, r = 0, len(nums) - 1
+    while l <= r:
+        m = mid(l, r)
+        if nums[m] == target:
+            first_found = m
+            break
+        elif nums[m] > target:
+            r = m - 1
+        else:
+            l = m + 1
+
+
+    is_leftmost = lambda index: index == 0 or nums[index-1] < target
+    is_rightmost = lambda index: index == len(nums)-1 or nums[index+1] > target
+    
+    if first_found:
+        if is_leftmost(first_found):
+            leftmost = first_found
+        else:
+            l, r = 0, first_found - 1
+            while l <= r:
+                m = mid(l, r)
+                if is_leftmost(m):
+                    leftmost = m
+                    break
+                elif nums[m] == target:
+                    r = m - 1
+                else:
+                    l = m + 1
+        if is_rightmost(first_found):
+            rightmost = first_found
+        else:
+            l, r = first_found + 1, len(nums) - 1
+            while l <= r:
+                m = mid(l, r)
+                if is_rightmost(m):
+                    rightmost = m
+                    break
+                elif nums[m] == target:
+                    l = m + 1
+                else:
+                    r = m - 1
+        return (rightmost - leftmost + 1) > len(nums)//2
+    
+    return False
--- python/patterns/test_modified_binary_search.py
+++ python/patterns/test_modified_binary_search.py
@@ -1,5 +1,5 @@
 import pytest
-import modified_binary_search
+from modified_binary_search import *
 
 @pytest.mark.parametrize("nums, target, expected", [
     ([1,6,8,10], 1, 0),
@@ -14,6 +14,15 @@ import modified_binary_search
     ([-30,-27,-8,-6,-1], -33, -1),
     ([0], 1, -1),
 ])
-
 def test_binary_search(nums, target, expected):
-    assert modified_binary_search.binary_search(nums, target) == expected
+    assert binary_search(nums, target) == expected
+
+@pytest.mark.parametrize("nums, target, expected", [
+    ([2, 4, 5, 5, 5, 5, 5, 6, 6], 5, True),
+    ([10, 100, 101, 101], 101, False),
+    ([1, 2, 3, 3, 3, 3, 10], 3, True),
+    ([1, 1, 2, 4, 4, 4, 6, 6], 4, False),
+    ([1, 1, 1, 2, 2], 1, True)
+])
+def test_is_majority(nums, target, expected):
+    assert is_majority(nums, target) == expected
