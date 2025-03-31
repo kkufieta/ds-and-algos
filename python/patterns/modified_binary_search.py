d2d4c0493eab5789474fa0aba7e006520ee2d0d7
kkufieta <katharina.kufieta@gmail.com>
2025-03-31 01:32:20 -0400

    Use find_index function

--- python/patterns/modified_binary_search.py
+++ python/patterns/modified_binary_search.py
@@ -20,20 +20,26 @@ def binary_search(nums, target):
 def first_and_last(nums, target):
     get_mid = lambda l, r: l + (r - l)//2
 
-    first_found, first, last = None, None, None
-    l, r = 0, len(nums) - 1
-    while l <= r:
-        mid = get_mid(l, r)
-        if nums[mid] == target:
-            first_found = mid
-            break
-        elif nums[mid] > target:
-            r = mid - 1
-        else:
-            l = mid + 1
-    is_leftmost = lambda index: index == 0 or (nums[index] == target and nums[index-1] < target)
-    is_rightmost = lambda index: index == len(nums) - 1 or (nums[index] == target and nums[index+1] > target)
+    def find_index(l, r, found_func, adjust_r_func):
+        while l <= r:
+            mid = get_mid(l, r)
+            if found_func(mid):
+                return mid
+            elif adjust_r_func(mid):
+                r = mid - 1
+            else:
+                l = mid + 1
+        return None
+
+
+    first, last = None, None
+    first_found = find_index(0, len(nums) - 1,
+                             lambda x: nums[x] == target,
+                             lambda x: nums[x] > target)
+
     if first_found:
+        is_leftmost = lambda index: index == 0 or (nums[index] == target and nums[index-1] < target)
+        is_rightmost = lambda index: index == len(nums) - 1 or (nums[index] == target and nums[index+1] > target)
         if is_leftmost(first_found):
             first = first_found
         else:
