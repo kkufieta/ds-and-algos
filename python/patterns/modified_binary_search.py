01bbe1cc99dd1d5769654addee9b5b1d1a06a656
kkufieta <katharina.kufieta@gmail.com>
2025-03-31 01:43:56 -0400

    Use first_and_last

--- python/patterns/modified_binary_search.py
+++ python/patterns/modified_binary_search.py
@@ -60,51 +60,10 @@ def is_majority(nums, target):
     if len(nums) == 0:
         return False
 
-    mid = lambda l, r: l + (r-l)//2
+    leftmost, rightmost = first_and_last(nums, target)
+    print(leftmost, rightmost)
 
-    first_found, leftmost, rightmost = None, None, None
-    l, r = 0, len(nums) - 1
-    while l <= r:
-        m = mid(l, r)
-        if nums[m] == target:
-            first_found = m
-            break
-        elif nums[m] > target:
-            r = m - 1
-        else:
-            l = m + 1
-
-
-    is_leftmost = lambda index: index == 0 or (nums[index] == target and nums[index-1] < target)
-    is_rightmost = lambda index: index == len(nums)-1 or (nums[index] == target and nums[index+1] > target)
-    
-    if first_found:
-        if is_leftmost(first_found):
-            leftmost = first_found
-        else:
-            l, r = 0, first_found - 1
-            while l <= r:
-                m = mid(l, r)
-                if is_leftmost(m):
-                    leftmost = m
-                    break
-                elif nums[m] == target:
-                    r = m - 1
-                else:
-                    l = m + 1
-        if is_rightmost(first_found):
-            rightmost = first_found
-        else:
-            l, r = first_found + 1, len(nums) - 1
-            while l <= r:
-                m = mid(l, r)
-                if is_rightmost(m):
-                    rightmost = m
-                    break
-                elif nums[m] == target:
-                    l = m + 1
-                else:
-                    r = m - 1
+    if leftmost != None and rightmost != None:
         return (rightmost - leftmost + 1) > len(nums)//2
     
     return False
