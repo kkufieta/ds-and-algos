5bacb9143de66057abc1f8a697a3474a579b4038
kkufieta <katharina.kufieta@gmail.com>
2025-03-31 10:40:17 -0400

    Simplify integer_sqrt

--- python/patterns/modified_binary_search.py
+++ python/patterns/modified_binary_search.py
@@ -79,11 +79,11 @@ def integer_sqrt(x):
     while l <= r:
         mid = l + (r-l)//2
         squared = lambda x: x**2
-        if squared(mid) <= x and squared(mid+1) > x:
+        if squared(mid) == x:
             return mid
         elif squared(mid) < x:
             l = mid + 1
         else:
             r = mid - 1
 
-    return x
+    return r
