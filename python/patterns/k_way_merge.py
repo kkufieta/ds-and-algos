201913c49efb83947be9998a4de1ee8714a8ddd2
kkufieta <katharina.kufieta@gmail.com>
2025-04-06 13:32:03 -0400

    Change code

--- python/patterns/k_way_merge.py
+++ python/patterns/k_way_merge.py
@@ -1,18 +1,13 @@
 def merge_sorted_in_place(nums1, m, nums2, n):
-    p_write = m + n - 1
-    m, n = m-1, n-1
-    while m >= 0 and n >= 0:
-        if nums1[m] > nums2[n]:
-            nums1[p_write] = nums1[m]
-            m -= 1
+    p1, p2 = m-1, n-1
+    for p_write in range(m + n - 1, -1, -1):
+        if p2 < 0:
+            break
+        if p1 >= 0 and nums1[p1] > nums2[p2]:
+            nums1[p_write] = nums1[p1]
+            p1 -= 1
         else:
-            nums1[p_write] = nums2[n]
-            n -= 1
-        p_write -= 1
-    
-    while n >= 0:
-        nums1[p_write] = nums2[n]
-        n -= 1
-        p_write -= 1
+            nums1[p_write] = nums2[p2]
+            p2 -= 1
 
     return nums1
