5b4a0894f549422a354c5e148f98eed2733683cb
kkufieta <katharina.kufieta@gmail.com>
2025-04-10 12:30:25 -0400

    Improve jump_game

--- python/patterns/greedy.py
+++ python/patterns/greedy.py
@@ -1,20 +1,11 @@
 
 def jump_game(nums):
-    if len(nums) == 0:
-        return False
-    target_i = len(nums) - 1
-    while target_i > 0:
-        i = target_i - 1
-        while i >= 0:
-            if (target_i - i) <= nums[i]:
-                target_i = i
-                break
-            else:
-                i -= 1
-                if i < 0:
-                    return False
-
-    return True
+    target, jump_start = len(nums) - 1, len(nums) - 2
+    while jump_start >= 0:
+        if nums[jump_start] >= target - jump_start:
+            target = jump_start
+        jump_start -= 1
+    return target == 0
 
 def jump_game_backtracking(nums):
     def jump(index):
