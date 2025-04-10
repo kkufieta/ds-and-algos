edcb95ee0b6f17594cb647fc908c27f4dcd05e66
kkufieta <katharina.kufieta@gmail.com>
2025-04-09 21:19:50 -0400

    Improve number_machines

--- python/patterns/two_heaps.py
+++ python/patterns/two_heaps.py
@@ -1,14 +1,11 @@
 import heapq
 
 def number_machines(tasks):
-    heapq.heapify(tasks)
+    tasks.sort()
     machines = []
-    max_num_machines = 0
-    add_task = lambda task: heapq.heappush(machines, task[1])
-    while len(tasks) > 0:
-        if len(machines) > 0 and machines[0] <= tasks[0][0]:
+    for start, end in tasks:
+        if machines and machines[0] <= start:
             heapq.heappop(machines)
-        add_task(heapq.heappop(tasks))
-        max_num_machines = max(max_num_machines, len(machines))
+        heapq.heappush(machines, end)
 
-    return max_num_machines
+    return len(machines)
