a4a45a0c340600f283ce9693f979a565382780c6
kkufieta <katharina.kufieta@gmail.com>
2025-04-09 21:10:54 -0400

    Rewrite number_machines

--- python/patterns/two_heaps.py
+++ python/patterns/two_heaps.py
@@ -1,18 +1,14 @@
 import heapq
 
 def number_machines(tasks):
-    if len(tasks) <= 1:
-        return len(tasks)
-
     heapq.heapify(tasks)
-
     machines = []
+    max_num_machines = 0
+    add_task = lambda task: heapq.heappush(machines, task[1])
     while len(tasks) > 0:
-        t = heapq.heappop(tasks)
+        if len(machines) > 0 and machines[0] <= tasks[0][0]:
+            heapq.heappop(machines)
+        add_task(heapq.heappop(tasks))
+        max_num_machines = max(max_num_machines, len(machines))
 
-        if len(machines) == 0 or t[0] < machines[0]:
-            heapq.heappush(machines, t[1])
-        else:
-            heapq.heapreplace(machines, t[1])
-
-    return len(machines)
+    return max_num_machines
