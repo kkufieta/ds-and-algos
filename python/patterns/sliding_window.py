b6f83036c85bbf81c23c46612c48e99ddf18d783
kkufieta <katharina.kufieta@gmail.com>
2025-04-20 20:41:33 -0400

    Refactor

--- python/patterns/sliding_window.py
+++ python/patterns/sliding_window.py
@@ -33,25 +33,7 @@ def find_repeated_dna_sequence_size_k(s, k):
 
 # Find repeated DNA sequences of size 10 (A, C, G, T)
 def find_repeated_dna_sequence_size_10(s):
-    k = 10
-    if len(s) < k:
-        return []
-    code = {'A': 0, 'C': 1, 'G': 2, 'T': 3,
-            0: 'A', 1: 'C', 2: 'G', 3: 'T'}
-    num = 0
-    for i in range(k):
-        num += code[s[i]] * 10**(k-1-i)
-    d = {num: 1}
-    result = []
-    for i in range(k, len(s)):
-        num = (num % 10**(k-1)) * 10 + code[s[i]]
-        if num not in d:
-            d[num] = 0
-        elif d[num] == 1:
-            result.append("".join([code[(num//10**(k-1-power))%10] for power in range(k)]))
-        d[num] += 1
-
-    return result
+    return find_repeated_dna_sequence_size_k(s, 10)
 
 def findRepeatedDnaSequences(s):
     if len(s) < 10:
