a5159182b23c1a510d1552afed73a75ee579a454
kkufieta <katharina.kufieta@gmail.com>
2025-04-23 22:20:39 -0400

    Simplify network_delay_time

--- python/patterns/graphs.py
+++ python/patterns/graphs.py
@@ -1,31 +1,30 @@
 def network_delay_time(times, n, k):
-    if len(times) < (n-1):
+    if len(times) < (n-1) or k > n or k < 1:
         return -1
 
-    t = {}
-    for start, end, delay in times:
-        if start in t:
-            t[start].append((start, end, delay))
-        else:
-            t[start] = [(start, end, delay)]
+    graph = {}
+    for source, target, delay in times:
+        if source not in graph:
+            graph[source] = []
+        graph[source].append((source, target, delay))
         
-    if k not in t:
+    if k not in graph:
         return -1
 
     delays = [-1] * n
-    stack = t[k]
+    delays[k-1] = 0
+    stack = graph[k]
     while len(stack) > 0:
-        start, end, delay = stack.pop()
-        if end != k and (delays[end-1] == -1 or delays[end-1] > delay):
-            delays[end-1] = delay
-            if end in t:
-                for s, e, d in t[end]:
-                    if e != k:
-                        stack.append((s, e, delay + d))
+        source, target, delay = stack.pop()
+        delay += delays[source-1]
+        if delays[target-1] == -1 or delays[target-1] > delay:
+            delays[target-1] = delay
+            if target in graph:
+                stack.extend(graph[target])
     
-    max_delay = delays[0]
-    for i, delay in enumerate(delays):
-        if delay == -1  and i != (k-1):
+    max_delay = 0
+    for delay in delays:
+        if delay == -1:
             return -1
         max_delay = max(max_delay, delay)
 
