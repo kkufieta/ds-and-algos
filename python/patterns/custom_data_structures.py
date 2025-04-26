aaa820465c6febad44277d1e3a9d49b644fea907
kkufieta <katharina.kufieta@gmail.com>
2025-04-25 20:34:30 -0400

    Implement initializer

--- python/patterns/custom_data_structures.py
+++ python/patterns/custom_data_structures.py
@@ -7,11 +7,19 @@
 
 class SnapshotArray:
 
-    def __init__(self, n):
-        pass
+    def __init__(self, length):
+        self.length = length
+        self.current = {}
+        self.snap_id = -1
+        self.snaps = {}
+        self.__snap_id = -1
+        self.__snaps = {}
 
     def _internal_state(self):
-        return []
+        a = [0] * self.length
+        for idx, val in self.current:
+            a[idx] = val
+        return a
 
     def set_value(self, idx, val):
         pass
