f2f95eab9ab16c80e9bb5e22881d56f9b5850d42
kkufieta <katharina.kufieta@gmail.com>
2025-04-25 20:27:55 -0400

    Add scaffolding

---
+++ python/patterns/custom_data_structures.py
@@ -0,0 +1,23 @@
+'''
+“SnapshotArray”: Constructor
+“set”: Set Value (idx, val)
+“snap”: Snapshot()
+“get” : Get Value (idx, Snap ID)
+'''
+
+class SnapshotArray:
+
+    def __init__(self, n):
+        pass
+
+    def _internal_state(self):
+        return []
+
+    def set_value(self, idx, val):
+        pass
+
+    def get_value(self, idx, snap_id):
+        return -1
+
+    def snapshot(self):
+        return -1
