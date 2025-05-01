3784a21187bbb09a233e2feb8172957aff095ef1
kkufieta <katharina.kufieta@gmail.com>
2025-05-01 14:08:38 -0400

    Refactor

--- python/patterns/lib/trie.py
+++ python/patterns/lib/trie.py
@@ -16,21 +16,21 @@ class Trie():
             node = node.children[ch]
         node.is_word = True
 
-    def search(self, string):
+    def _search_prefix_node(self, prefix):
         node = self.root
-        for ch in string:
+        for ch in prefix:
             if ch not in node.children:
-                return False
+                return None
             node = node.children[ch]
-        return node.is_word
+        return node
+
+    def search(self, string):
+        node = self._search_prefix_node(string)
+        return node != None and node.is_word
 
     def search_prefix(self, string):
-        node = self.root
-        for ch in string:
-            if ch not in node.children:
-                return False
-            node = node.children[ch]
-        return True
+        node = self._search_prefix_node(string)
+        return node != None
 
     def remove_characters(self, string_to_delete):
         if not self.search(string_to_delete):
