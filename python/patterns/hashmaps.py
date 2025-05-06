29f60b8853bf275393d994ff65099dfbf0272334
kkufieta <katharina.kufieta@gmail.com>
2025-05-06 16:55:46 -0400

    Add group_anagrams

--- python/patterns/hashmaps.py
+++ python/patterns/hashmaps.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 def permutation_is_palindrome(s):
     d = {}
     odd_count = 0
@@ -11,3 +13,13 @@ def permutation_is_palindrome(s):
         else:
             odd_count -= 1
     return odd_count <= 1
+
+
+def group_anagrams(words):
+    groups = defaultdict(list)
+    for w in words:
+        count = [0]*26
+        for l in w:
+            count[ord(l)-ord('a')] += 1
+        groups[tuple(count)].append(w)
+    return list(groups.values())
