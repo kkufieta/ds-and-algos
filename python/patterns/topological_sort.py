6dc3bc1dc245ca0f65df8aca6bf4e9d310c5e136
kkufieta <katharina.kufieta@gmail.com>
2025-05-15 13:13:13 -0400

    Remove unnecessary queue

--- python/patterns/topological_sort.py
+++ python/patterns/topological_sort.py
@@ -1,4 +1,4 @@
-from collections import deque, defaultdict
+from collections import defaultdict
 
 def compilation_order(dependencies):
     in_degree, graph = defaultdict(int), defaultdict(list)
@@ -12,14 +12,12 @@ def compilation_order(dependencies):
         in_degree[child] += 1
         graph[parent].append(child)
 
-    compilation_order = []
-    queue = deque([node for node in in_degree if in_degree[node] == 0])
-    while len(queue) != 0:
-        node = queue.popleft()
-        compilation_order.append(node)
-        for child in graph[node]:
+    i, compilation_order = 0, [node for node in in_degree if in_degree[node] == 0]
+    while i < len(compilation_order):
+        for child in graph[compilation_order[i]]:
             in_degree[child] -= 1
             if in_degree[child] == 0:
-                queue.append(child)
+                compilation_order.append(child)
+        i += 1
 
     return compilation_order
