d0b5fc4071e073fbd270a91bfcacf33a0c1869b7
kkufieta <katharina.kufieta@gmail.com>
2025-05-20 16:43:46 -0400

    Reduce complexity

--- python/patterns/dynamic_programming.py
+++ python/patterns/dynamic_programming.py
@@ -1,12 +1,9 @@
 def knapsack_0_1(capacity, weights, values):
     vals = [0] * (capacity + 1)
-
-    for w, v in zip(weights[:-1], values[:-1]):
+    for w, v in zip(weights, values):
         for c in range(capacity, w-1, -1):
             vals[c] = max(vals[c], v + vals[c-w])
-
-    return max(vals[-1], values[-1] + vals[-1 - weights[-1]])
-
+    return vals[-1]
 
 def fibonacci_recursive_naive(num):
     if num <= 1:
