a47dbe6509abb066de3e049b282d3b0ff9f68f53
kkufieta <katharina.kufieta@gmail.com>
2025-05-20 14:52:15 -0400

    Add recursive & sequential fibonacci

--- python/patterns/dynamic_programming.py
+++ python/patterns/dynamic_programming.py
@@ -6,3 +6,20 @@ def knapsack_0_1(capacity, weights, values):
             vals[c] = max(vals[c], v + vals[c-w])
 
     return max(vals[-1], values[-1] + vals[-1 - weights[-1]])
+
+
+def fibonacci_recursive_naive(num):
+    if num <= 1:
+        return num
+    else:
+        return fibonacci_recursive_naive(num-1) + fibonacci_recursive_naive(num-2)
+
+def fibonacci_sequential(num):
+    if num <= 1:
+        return num
+    i, n1, n2 = 2, 0, 1
+    for i in range(1, num):
+        tmp = n1 + n2
+        n1 = n2
+        n2 = tmp
+    return n2
