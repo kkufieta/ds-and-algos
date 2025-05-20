ecb16b88993786c0313cd094398693836538f528
kkufieta <katharina.kufieta@gmail.com>
2025-05-20 16:40:49 -0400

    Add tests

--- python/patterns/test_dynamic_programming.py
+++ python/patterns/test_dynamic_programming.py
@@ -7,8 +7,10 @@ from dynamic_programming import *
     (3 , [2] , [3], 3),
     (10 , [3,6,10,7,2] , [12,10,15,17,13], 30),
     (30 , [10,20,30] , [22,33,44], 55),
+    (5 , [1, 2, 3, 5] , [10, 5, 4, 8], 15),
+    (50 , [10, 20, 30] , [600, 100, 120], 720),
+    (26 , [24, 10, 10, 7] , [24, 16, 16, 11], 32),
 ])
-
 def test_knapsack_0_1(capacity, weights, values, expected):
     assert knapsack_0_1(capacity, weights, values) == expected
 
