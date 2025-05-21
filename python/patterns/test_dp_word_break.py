97b2a6494a21c819d5af5585c785f536134c9fe9
kkufieta <katharina.kufieta@gmail.com>
2025-05-21 07:22:42 -0400

    Add tests

--- python/patterns/test_dp_word_break.py
+++ python/patterns/test_dp_word_break.py
@@ -2,12 +2,37 @@ import pytest
 from dp_word_break import *
 
 @pytest.mark.parametrize("s, word_dict, expected", [
-    ("magiclly" , ["ag","al","icl","mag","magic","ly","lly"], ["mag icl ly","magic lly"]),
-    ("raincoats" , ["rain","oats","coat","s","rains","oat","coats","c"], ["rain c oat s","rain c oats","rain coat s","rain coats"]),
-    ("highway" , ["crash","cream","high","highway","low","way"], ["high way","highway"]),
-    ("robocat" , ["rob","cat","robo","bo","b"], ["robo cat"]),
-    ("cocomomo" , ["co","mo","coco","momo"], ["co co mo mo","co co momo","coco mo mo","coco momo"]),
+    ("magiclly",
+     ["ag","al","icl","mag","magic","ly","lly"],
+     ["mag icl ly","magic lly"]),
+    ("raincoats",
+     ["rain","oats","coat","s","rains","oat","coats","c"],
+     ["rain c oat s","rain c oats","rain coat s","rain coats"]),
+    ("highway",
+     ["crash","cream","high","highway","low","way"],
+     ["high way","highway"]),
+    ("robocat",
+     ["rob","cat","robo","bo","b"],
+     ["robo cat"]),
+    ("cocomomo",
+     ["co","mo","coco","momo"],
+     ["co co mo mo","co co momo","coco mo mo","coco momo"]),
+    ("catsanddog",
+     ["cat", "and", "cats", "sand", "dog"],
+     ["cats and dog", "cat sand dog"]),
+     ("catsandog",
+     ["cat", "and", "cats", "sand", "dog"],
+     []),
+     ("pineapplepenapple",
+     ["apple", "pen", "applepen", "pine", "pineapple"],
+     ["pine apple pen apple", "pineapple pen apple", "pine applepen apple"]),
+     ("cattos",
+     ["cats", "cat", "to", "os"],
+     []),
+     ("superdogs",
+     ["super", "dogs", "dog", "s"],
+     ["super dogs", "super dog s"])
 ])
 
 def test_word_break(s, word_dict, expected):
-    assert word_break(s, word_dict) == expected
+    assert sorted(word_break(s, word_dict)) == sorted(expected)
