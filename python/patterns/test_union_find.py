219e95f8727a5c2a567f476e34e97244ea23e793
kkufieta <katharina.kufieta@gmail.com>
2025-05-21 16:15:04 -0400

    Add test

--- python/patterns/test_union_find.py
+++ python/patterns/test_union_find.py
@@ -7,6 +7,7 @@ from union_find import *
     ([[1,2],[2,3],[3,4],[1,4],[1,5]], [1,4]),
     ([[1,2],[1,3],[1,4],[2,3]], [2,3]),
     ([[1,2],[1,3],[1,4],[1,5],[2,5]], [2,5]),
+    ([[1, 2], [2, 3], [2, 4], [2, 5], [3, 5]], [3,5]),
 ])
 def test_redundant_connection(edges, expected):
     assert redundant_connection(edges) == expected
